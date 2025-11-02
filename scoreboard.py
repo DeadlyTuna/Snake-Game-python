9299f416bb4edead312bfbaf4ddc36bae9e29e64
DeadlyTuna <harshvardhannbhusari@gmail.com>
2025-11-03 02:37:55 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -5,27 +5,35 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
         self.hideturtle()
-        self.update_score()
+        self.load_high_score()
+        self.update_scoreboard()
 
-    def update_score(self):
+    def load_high_score(self):
+        try:
+            with open("data.txt") as data:
+                self.high_score = int(data.read())
+        except FileNotFoundError:
+            self.high_score = 0
+        except ValueError:
+            self.high_score = 0
+
+    def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}  High Score: {self.highscore}", align="center", font=("Arial", 24, "normal"))
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 24, "normal"))
 
-    def inc_score(self):
+    def increase_score(self):
         self.score += 1
-        self.update_score()
+        self.update_scoreboard()
 
     def reset(self):
-        if self.score > self.highscore:
-            self.highscore = self.score
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
-        self.update_score()
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("Arial", 24, "normal"))
+        self.update_scoreboard()
