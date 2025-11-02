9d71b7e4b45ee09d1a2927a23c239f23190ce48a
DeadlyTuna <harshvardhannbhusari@gmail.com>
2025-11-03 02:37:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
 from food import Food
@@ -20,26 +20,23 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-game_is_on = True
+is_game_on = True
 
-while game_is_on:
+while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.inc_score()
+        scoreboard.increase_score()
 
-    # Detect collision with wall
     if (snake.head.xcor() > 280 or snake.head.xcor() < -280 or
         snake.head.ycor() > 280 or snake.head.ycor() < -280):
         scoreboard.reset()
         snake.reset()
 
-    # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             scoreboard.reset()
