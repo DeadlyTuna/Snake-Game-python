f789157941c49d65d718b82a2d9045e17c2c95fa
DeadlyTuna <harshvardhannbhusari@gmail.com>
2025-11-03 02:41:07 +0530

    Update food.py

--- food.py
+++ food.py
@@ -6,12 +6,12 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)  # Correct method to shrink size
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
         self.refresh()
 
     def refresh(self):
-        ran_x = random.randint(-280, 280)
-        ran_y = random.randint(-280, 280)
-        self.goto(ran_x, ran_y)
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
